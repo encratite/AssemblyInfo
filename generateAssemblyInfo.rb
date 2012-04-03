d56fbc3ed35b83abf554659b4ac6fd3cb49b2b5a
void <git.email.address@googlemail.com>
2012-04-03 20:30:39 +0200

    Cosmetics

--- generateAssemblyInfo.rb
+++ generateAssemblyInfo.rb
@@ -56,48 +56,52 @@ def joinPaths(*arguments)
   return path
 end
 
-targets = [
-  'x86',
-  'Debug',
-  'Release',
-  'bin',
-]
+def generateAssemblyInfo
+  targets = [
+    'x86',
+    'Debug',
+    'Release',
+    'bin',
+  ]
 
-modifiedPath = Dir.pwd
-targets.each do |target|
-  modifiedPath = modifiedPath.gsub("/#{target}", '')
-end
-properties = joinPaths(modifiedPath, 'Properties')
-inputPath = joinPaths(properties, 'AssemblyInfo.template.cs')
-outputPath = joinPaths(properties, 'AssemblyInfo.cs')
-
-lines = nil
-status = IO.popen('git log --pretty=oneline') do |io|
-  input = io.read.strip
-  lines = input.split("\n")
-end
-pattern = /^[^ ].*? \((\d+)\):$/
-revision = lines.size
-
-lines = readLines(inputPath)
-if lines == nil
-  puts "Unable to read file #{inputPath}"
-  exit
-end
-
-pattern = /^\[assembly: AssemblyVersion\("(\d+)\.(\d+).+?"\)\]$/
-
-lines.map! do |line|
-  match = line.match(pattern)
-  if match != nil
-    major = match[1].to_i
-    minor = match[2].to_i
-    build = 0
-    line = "[assembly: AssemblyVersion(\"#{major}.#{minor}.0.#{revision}\")]"
-    puts line
+  modifiedPath = Dir.pwd
+  targets.each do |target|
+    modifiedPath = modifiedPath.gsub("/#{target}", '')
   end
-  line
+  properties = joinPaths(modifiedPath, 'Properties')
+  inputPath = joinPaths(properties, 'AssemblyInfo.template.cs')
+  outputPath = joinPaths(properties, 'AssemblyInfo.cs')
+
+  lines = nil
+  status = IO.popen('git log --pretty=oneline') do |io|
+    input = io.read.strip
+    lines = input.split("\n")
+  end
+  pattern = /^[^ ].*? \((\d+)\):$/
+  revision = lines.size
+
+  lines = readLines(inputPath)
+  if lines == nil
+    puts "Unable to read file #{inputPath}"
+    exit
+  end
+
+  pattern = /^\[assembly: AssemblyVersion\("(\d+)\.(\d+).+?"\)\]$/
+
+  lines.map! do |line|
+    match = line.match(pattern)
+    if match != nil
+      major = match[1].to_i
+      minor = match[2].to_i
+      build = 0
+      line = "[assembly: AssemblyVersion(\"#{major}.#{minor}.0.#{revision}\")]"
+      puts line
+    end
+    line
+  end
+
+  output = lines.join("\n")
+  writeFile(outputPath, output)
 end
 
-output = lines.join("\n")
-writeFile(outputPath, output)
+generateAssemblyInfo
